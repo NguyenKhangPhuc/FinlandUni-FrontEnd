cf8cffc967314806515c433dbeb29ba641856dbd
Khang Phuc Nguyen <nkpnkpnkp2005@gmail.com>
2025-04-27 21:24:45 +0300

    Services: Adding error handling for catching the error if there are problems fetching the data

--- services/UniService.tsx
+++ services/UniService.tsx
@@ -6,18 +6,39 @@ const url: string = 'http://localhost:3005/api'
 
 export const getUniversities = async (): Promise<Array<University>> => {
     /// Fetch all the universities in the database
-    const response = await axios.get<Array<University>>(url + '/universities');
-    return response.data;
+    try {
+        const response = await axios.get<Array<University>>(url + '/universities');
+        return response.data;
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(`Failed to fetch universities. Server error: ${error?.response?.data.error}`);
+        }
+        throw new Error(`Failed to fetch universities. Error unknown`)
+    }
 }
 
 export const getUniversityById = async (uniID: string): Promise<University> => {
     /// Fetch the specific university by the ID
-    const response = await axios.get<University>(url + `/universities/${uniID}`)
-    return response.data
+    try {
+        const response = await axios.get<University>(url + `/universities/${uniID}`)
+        return response.data
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(`Failed to specific university. Server error: ${error?.response?.data.error}`);
+        }
+        throw new Error(`Failed to specific university. Error unknown`)
+    }
 }
 
 export const getMajorsByUni = async (uniID: string): Promise<Array<Major>> => {
     /// Fetch all the majors of a specific university by its id
-    const response = await axios.get<Array<Major>>(url + `/universities/${uniID}/majors`)
-    return response.data
+    try {
+        const response = await axios.get<Array<Major>>(url + `/universities/${uniID}/majors`)
+        return response.data
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(`Failed to fetch the majors by the specific university id. Server error: ${error?.response?.data.error}`)
+        }
+        throw new Error(`Failed to fetch the majors by the specific university id`)
+    }
 }
--- src/app/university/[id]/page.tsx
+++ src/app/university/[id]/page.tsx
@@ -11,20 +11,19 @@ export default async function UniversityDetails({ params }: { params: { id: stri
     const university = await getUniversityById(id)
     const majors = await getMajorsByUni(id)
 
-
     return (
         <div className="w-full flex flex-col items-center">
-            <div className="header p-3 text-indigo-500">{university.name}</div>
+            <div className="header p-3 text-indigo-500">{university?.name}</div>
             <div className="xl:text-[15px] w-full bg-gray-800 text-white p-3 opacity-70 flex justify-around text-[10px] gap-3">
-                <div className="text-center">Location: {university.city}</div>
-                <div className="text-center">Type: {university.type}</div>
-                <div className="text-center">Tuition: {university.tuition}</div>
-                <div className="text-center">Scholarship: {university.scholarship}</div>
+                <div className="text-center">Location: {university?.city}</div>
+                <div className="text-center">Type: {university?.type}</div>
+                <div className="text-center">Tuition: {university?.tuition}</div>
+                <div className="text-center">Scholarship: {university?.scholarship}</div>
             </div>
             <div className="header p-5 text-white">Overview</div>
-            <div className="lg:w-1/2 w-full  text-white p-5 bg-gray-800 rounded-lg">{university.overview}</div>
+            <div className="lg:w-1/2 w-full  text-white p-5 bg-gray-800 rounded-lg">{university?.overview}</div>
             <div className="header p-5 text-white">Majors</div>
-            <Majors majors={majors} id={id} fieldOptions={university.fieldOptions} />
+            <Majors majors={majors} id={id} fieldOptions={university?.fieldOptions} />
         </div>
     )
 }
